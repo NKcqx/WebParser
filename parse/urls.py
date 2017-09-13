ccb02205e66fa50b218876084f1a9ec50810cc25
陈齐翔 <892670992@qq.com>
2017-09-13 17:36:52 +0800

    Integrate Mercury parser, refs #2

--- parse/urls.py
+++ parse/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^parse/$', views.parseWeb, name = 'parseWeb')
 ]
--- parse/views.py
+++ parse/views.py
@@ -1,5 +1,43 @@
+import json
 from django.shortcuts import render
-
+from mercury_parser import ParserAPI
+from django.http import HttpResponse
 # Create your views here.
+def parse (url):
+    mercury = ParserAPI(api_key='cVwutZHUtwKpfsa1Dn2f93QDPdM6N5WKCCQIiVpb')
+    return mercury.parse('http://www.cnblogs.com/leoo2sk/archive/2010/09/17/naive-bayesian-classifier.html')
+
 def index (request):
-    return render(request, 'index.html', {'test': 'test string'})
+    return render(request, 'index.html', {'title':' result.title', 'content': 'result.content'})
+def parseWeb (request):
+    url = request.GET.get('url')
+    result = parse(url)
+    print('result', json.dumps({
+                'title': result.title,
+                'date_published': result.date_published.strftime('%Y-%-m-%d %H:%m:%S'),
+                'lead_image_url': result.lead_image_url,
+                'def': result.dek,
+                'url': result.url,
+                'domain': result.domain,
+                'excerpt': result.excerpt,
+                'word_count': result.word_count,
+                'direction': result.direction,
+                'total_pages': result.total_pages,
+                'rendered_pages': result.rendered_pages,
+                'next_page_url': result.next_page_url
+            }))
+    return HttpResponse(json.dumps({
+                'title': result.title,
+                'content': result.content,
+                'date_published': result.date_published.strftime('%Y-%-m-%d %H:%m:%S'),
+                'lead_image_url': result.lead_image_url,
+                'def': result.dek,
+                'url': result.url,
+                'domain': result.domain,
+                'excerpt': result.excerpt,
+                'word_count': result.word_count,
+                'direction': result.direction,
+                'total_pages': result.total_pages,
+                'rendered_pages': result.rendered_pages,
+                'next_page_url': result.next_page_url
+            }))
